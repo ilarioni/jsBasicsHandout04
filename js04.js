50fdb1e6b4ca556ca3b6c88ba4ce1891eccd8bf6
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-04 20:34:05 +0100

    map()

--- js04.js
+++ js04.js
@@ -91,3 +91,17 @@ console.log(points);
 // lastIndexOf()
 // find()
 // findIndex()
+
+var txt = "";
+var numbers = [45,4,9,16,25];
+// numbers.forEach(myFunction);
+// function myFunction(value) {
+//    txt = txt + value + "<br>";
+// }
+
+var numbers1 = [45,4,9,16,25];
+var numbers2 = numbers1.map(myFunction);
+function myFunction(value) {
+    return value * 2;
+}
+console.log(numbers2);
