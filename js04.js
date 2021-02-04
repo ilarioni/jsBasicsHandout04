6921dd2a3dc3bdb99dbb3de2de0afb5d9e85369e
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-04 20:27:32 +0100

    pop & push

---
+++ js04.js
@@ -0,0 +1,12 @@
+// Array Methods and Array Sorting
+// toString() join()
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.length);
+console.log(fruits.toString());
+console.log(fruits.join(" - "));
+
+// pop() & push() - last element on the right side
+console.log(fruits.pop());
+console.log(fruits);
+console.log(fruits.push("Kiwi"));
+console.log(fruits);
